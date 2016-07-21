3277e15ba86f40cf712dc94c4ade691062baeebf
Dominic Cullen <dcullen97@gmail.com>
2016-07-21 17:58:52 +0100

    Added showing of companies, need to add field to edit additional info

--- resources/views/company/show.blade.php
+++ resources/views/company/show.blade.php
@@ -1,15 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
 
-            @foreach($companies as $company)
-            <div class="panel panel-default">
+                <div class="panel panel-default">
 
                     <div class="panel-heading">
-                        <a href={{ url('company', $company->id) }}>
+                        <a href="{{ url('company', $company->id) }}">
                             {{ $company->company_name }}
                         </a>
                     </div>
@@ -28,9 +27,10 @@
                             {{ $company->additional_info }}
                         </div>
                     </div>
+
+                </div>
+
             </div>
-            @endforeach
         </div>
     </div>
-</div>
 @endsection
